831624387705b2ceb9dfc1439d178956e3b1a2bd
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-15 13:28:19 +0200

    feat: lesson5 task webpack-config

---
+++ lesson5/webpack-config/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: {
+    dashboard: './src/dashboard/index.js',
+    profile: './src/profile/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist',
+  },
+};
