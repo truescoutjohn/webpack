3747191d22348704ae3dae9d5ff566209dde7057
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-15 15:48:47 +0200

    feat: lesson6 task webpack-css-loader

---
+++ lesson6/webpack-css-loader/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.s?css$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      {
+        test: /.(png|jpg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8,
+            name: '[name].[ext]',
+            outputName: 'images',
+          },
+        },
+      },
+    ],
+  },
+};
