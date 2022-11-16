9af2c0b5802a22a2141857100b73ac20af68b1ad
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-16 10:26:25 +0200

    feat: lesson7 task webpack-plugin

---
+++ lesson7/webpack-plugin/webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.s?css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+      { test: /\.js$/, use: ['babel-loader'] },
+      {
+        test: /.(png|jpg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: '[name].[ext]',
+            outputPath: 'images',
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+  ],
+};
